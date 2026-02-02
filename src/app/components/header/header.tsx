aecbe68b91151b2d7f49850948c4e960d5a14965
Erli Junior <erlijunior@outlook.com>
2026-02-02 20:39:26 -0300

    Adiciona z-index ao Header e organiza a estrutura do layout

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Header = () => {
 	return (
-		<header className='fixed top-0 left-0 w-full p-4 bg-white shadow-md'>
+		<header className='fixed top-0 left-0 w-full p-4 bg-white shadow-md z-10'>
 			<nav className='flex items-center justify-between max-w-7xl mx-auto'>
 				<Link href='/'>
 					<h1 className='text-xl font-bold uppercase'>Next.js Ecommerce</h1>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Header from './components/header/header'
+import { Footer } from './components/footer/footer'
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -26,10 +27,9 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<main className='h-screen py-16'>
-					<Header />
-					{children}
-				</main>
+				<Header />
+				<main className='min-h-screen py-16'>{children}</main>
+				<Footer />
 			</body>
 		</html>
 	)
